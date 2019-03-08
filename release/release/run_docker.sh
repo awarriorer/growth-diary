2be06d25ff73c5643f61edab9b76a72078d9984e
awarriorer <awarriorer@163.com>
2019-03-08 12:32:57 +0800

    merge

--- release/release/run_docker.sh
+++ release/release/run_docker.sh
@@ -5,5 +5,4 @@
 docker rm -f my-blog-server
 
 # docker run container
-# docker run -it -d -e WEB_BASE_SYSTEM="release" --name="web-server" -p 8080:1122 web-server
 docker run -it -d --name="my-blog-server" --expose=8009 my-blog-server
